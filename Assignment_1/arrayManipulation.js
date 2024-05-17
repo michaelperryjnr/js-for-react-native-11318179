69a69e1d75782035403282ed4b5f7f0b0068e7e8
michaelperryjnr <michaelperryt97@gmail.com>
2024-05-17 21:25:57 +0000

    Completed Task 2

--- Assignment_1/arrayManipulation.js
+++ Assignment_1/arrayManipulation.js
@@ -17,3 +17,20 @@ let numArrayTest = [1,2,3,4,5,6,];
 console.log(processArray(numArrayTest));
 
 
+/**Task 2*/
+function formatArrayStrings(arrayOfStrings, processedArray) {
+    if(arrayOfStrings.length <= 0 || processedArray.length <= 0 || arrayOfStrings.length !== processedArray.length) return;
+
+    let formattedStrings = arrayOfStrings.map((string, index)=> {
+        if(processedArray[index] % 2 === 0){
+            return string.toUpperCase();
+        }
+        return string.toLowerCase();
+    });
+    return formattedStrings;
+};
+
+/**Task 2 Test*/
+let processedArray = processArray(numArrayTest);
+let rawStrings = ["miChael", "perRy", "Nii", "jOmO", "oSaH", "TeTTey"];
+console.log(formatArrayStrings(rawStrings, processedArray));
